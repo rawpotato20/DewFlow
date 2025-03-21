219e8882e671cdc138903ba3e5644a9887833262
Oscar2006 <oskarascharciuk@gmail.com>
2025-03-21 19:20:48 +0200

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
-  <div className="flex flex-col justify-center items-center h-screen">
-    <h1 className="mb-5 text-2xl">Welcome, to Next.Js!</h1>
-    <Image src="/next.svg" alt="Next Logo" width={400} height={1000} />
-  </div>
-  </>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className="mb-5 text-2xl">Welcome, to Next.Js!</h1>
+        <Image src="/next.svg" alt="Next Logo" width={400} height={1000} />
+      </div>
+    </>
   );
 }
