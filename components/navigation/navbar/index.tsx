b24dd9777772f5fc92a52596221c1ede87d70d25
Oscar2006 <oskarascharciuk@gmail.com>
2025-03-23 20:51:48 +0200

    Create sign-in & sign-up

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           alt="DevFlow Logo"
         />
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dew<span className="text-primary-500">Flow</span>
+          Dev<span className="text-primary-500">Flow</span>
         </p>
       </Link>
       <p>Global Search</p>
