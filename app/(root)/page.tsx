4dded8a2c13f1fc82b6e22def3adebf5aa98f81a
Oscar2006 <oskarascharciuk@gmail.com>
2025-03-26 17:02:11 +0200

    Implement SignIn & SignUp forms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,15 +10,6 @@ export default async function Home() {
   return (
     <>
       <h1 className="h1-bold">Hello and Welcome to Next.js!</h1>;
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 }
