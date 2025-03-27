977a9f992c49c199e3a065cc3800807551b164c8
Oscar2006 <oskarascharciuk@gmail.com>
2025-03-27 12:58:51 +0200

    Implement Right Sidebar

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -45,7 +45,7 @@ const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
             <p
               className={cn(
                 isActive ? "base-bold" : "base-medium",
-                !isMobileNav && "max-lg: hidden"
+                !isMobileNav && "max-lg:hidden"
               )}
             >
               {item.label}
