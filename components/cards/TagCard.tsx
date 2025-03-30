b5f14a7a9165c34998b8264e8748ac71d741251d
Oscar2006 <oskarascharciuk@gmail.com>
2025-03-30 12:24:27 +0300

    Implement Tag Input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import React from "react";
 import { Badge } from "../ui/badge";
 import { getDevIconClassName } from "@/lib/utils";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 interface Props {
   _id: string;
@@ -10,25 +12,62 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+  remove,
+  isButton,
+  handleRemove,
+}: Props) => {
   const iconClass = getDevIconClassName(name);
 
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+  const Content = (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            width={12}
+            height={12}
+            alt="close icons"
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
 
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
       )}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button
+        onClick={(e) => e.preventDefault()}
+        className="flex justify-between gap-2"
+      >
+        {Content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        {Content}
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -17,6 +17,8 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { MDXEditorMethods } from "@mdxeditor/editor";
 import dynamic from "next/dynamic";
+import { z } from "zod";
+import TagCard from "../cards/TagCard";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   // Make sure we turn SSR off
@@ -26,7 +28,7 @@ const Editor = dynamic(() => import("@/components/editor"), {
 const QuestionForm = () => {
   const editorRef = useRef<MDXEditorMethods>(null);
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
       title: "",
@@ -35,7 +37,48 @@ const QuestionForm = () => {
     },
   });
 
-  function handleCreateQuestion() {}
+  function handleInputKeyDown(
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: { value: string[] }
+  ) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const tagInput = e.currentTarget.value.trim();
+
+      if (tagInput && tagInput.length < 15 && !field.value.includes(tagInput)) {
+        form.setValue("tags", [...field.value, tagInput]);
+        e.currentTarget.value = "";
+        form.clearErrors("tags");
+      } else if (tagInput.length > 15) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag shouuld be less than 15 charaters",
+        });
+      } else if (field.value.includes(tagInput)) {
+        form.setError("tags", {
+          type: "manual",
+          message: "Tag already exists.",
+        });
+      }
+    }
+  }
+
+  function handleTagRemove(tag: string, field: { value: string[] }) {
+    const newTags = field.value.filter((t) => t !== tag);
+
+    form.setValue("tags", newTags);
+
+    if (newTags.length === 0) {
+      form.setError("tags", {
+        type: "manual",
+        message: "Tags are required.",
+      });
+    }
+  }
+
+  function handleCreateQuestion(data: z.infer<typeof AskQuestionSchema>) {
+    console.log(data);
+  }
 
   return (
     <Form {...form}>
@@ -101,15 +144,29 @@ const QuestionForm = () => {
               <FormControl>
                 <div>
                   <Input
-                    {...field}
                     className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] border"
                     placeholder="Add Tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
-                  Tags
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 flex-wrap gap-2.5">
+                      {field?.value?.map((tag: string) => (
+                        <TagCard
+                          key={tag}
+                          _id={tag}
+                          name={tag}
+                          compact
+                          remove
+                          isButton
+                          handleRemove={() => handleTagRemove(tag, field)}
+                        />
+                      ))}
+                    </div>
+                  )}
                 </div>
               </FormControl>
               <FormDescription className="body-regular text-light-500 mt-2.5">
-                Add up to 3 tags to describe what your question is about. You
+                Add up to 5 tags to describe what your question is about. You
                 need to press "Enter" to add a tag.
               </FormDescription>
               <FormMessage />
